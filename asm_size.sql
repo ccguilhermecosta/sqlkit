0050d1e3e82e04c2de0f64cfdb0f90b0322f5859
Guilherme Costa <ccguilherme.costa@gmail.com>
2023-08-11 10:51:00 -0300

    Adjusting the ASM script to account for redundancy

--- asm_size.sql
+++ asm_size.sql
@@ -1,11 +1,15 @@
-set linesize 300
-col ocupado format a15
-select 
-	name,
-	state,
-	total_mb/1024 Total_GB,
-	free_mb/1024 Free_GB,
-	to_char((((TOTAL_MB - FREE_MB) / TOTAL_MB)  * 100),999.99)|| ' %' Ocupado,
-	type 
-from 
-	v$asm_diskgroup; 
+SET SQLFORMAT
+COL NAME HEADING "Disksgroup"
+COL TYPE HEADING "Redundancy"
+COL TOTAL_GB HEADING "Total GB" FORMAT 999,999,999,999  
+COL FREE_GB  HEADING "Free GB"  FORMAT 999,999,999,999 
+COL PERCFREE HEADING "Free %"   FORMAT 999.99 
+
+SELECT NAME, TYPE, TOTAL_GB, FREE_GB, ROUND(FREE_GB/TOTAL_GB*100,2) AS PERCFREE
+FROM (
+	SELECT NAME, 
+	       TYPE,
+		   ( TOTAL_MB / DECODE(TYPE,'HIGH',3,'NORMAL',2,1) ) / 1024 AS TOTAL_GB, 
+		   USABLE_FILE_MB/1024 AS FREE_GB
+	FROM V$ASM_DISKGROUP
+);
